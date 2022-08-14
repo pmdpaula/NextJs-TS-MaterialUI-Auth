56668cde24945144127a22177f3474896b8e5608
Pedro de Paula (remoto) <pmdpaula@yahoo.com.br>
2022-08-14 11:10:53 -0300

    refactor(types): types definitions

--- types/context.d.ts
+++ types/context.d.ts
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { Error, UserProfile } from '@auth0/nextjs-auth0';
 import { Theme } from '@mui/system';
 
 export interface websitePageContextProps {
@@ -7,4 +8,7 @@ export interface websitePageContextProps {
   setCurrentTheme: Dispatch<SetStateAction<Theme>>;
   headTitle: string;
   setHeadTitle: Dispatch<SetStateAction<Theme>>;
+  userLogged: UserProfile | undefined;
+  userError: Error | undefined;
+  isUserLoginLoading: boolean;
 }
